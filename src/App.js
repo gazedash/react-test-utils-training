7de11e812e8f56ef8bda9739cda5ce3ee5b399b3
shoegazerwithak <skrtroot@protonmail.com>
2017-10-31 12:55:51 +0300

    Test click, try enzyme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    background: "grey"
+  };
+
+  handleClick = () => {
+    this.props.onClick();
+    this.setState({ background: "red" });
+  };
   render() {
     return (
-      <div className="App">
+      <div style={{ background: this.state.background }} className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +21,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleClick} />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactTestUtils from "react-dom/test-utils";
 import ShallowRenderer from "react-test-renderer/shallow"; // ES6
+
+import { mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
 import App from "./App";
 
 it("renders without crashing", () => {
@@ -22,4 +29,23 @@ it("renders expected children", () => {
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   );
+});
+
+it("simulates click and changes background", () => {
+  const mockFunction = jest.fn();
+  const element = ReactTestUtils.renderIntoDocument(
+    <App onClick={mockFunction} />
+  );
+  const app = ReactDOM.findDOMNode(element);
+  const button = app.children[2];
+  ReactTestUtils.Simulate.click(button);
+  expect(mockFunction).toHaveBeenCalled();
+  expect(app.style.background).toBe("red");
+});
+
+it("enzyme: simulate click", () => {
+  const mockFunction = jest.fn();
+  const element = mount(<App onClick={mockFunction} />);
+  element.find("button").simulate("click");
+  expect(mockFunction).toHaveBeenCalled();
 });
