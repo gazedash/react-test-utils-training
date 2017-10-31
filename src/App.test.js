2da85f5ef8220519186fd0ec36d6ae43106e4f31
shoegazerwithak <skrtroot@protonmail.com>
2017-10-31 12:24:33 +0300

    Test render results

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import ShallowRenderer from "react-test-renderer/shallow"; // ES6
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+it("renders expected children", () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+  const result = renderer.getRenderOutput();
+
+  expect(result.type).toBe("div");
+  expect(result.props.children[1].props.children[0]).toBe(
+    "To get started, edit "
+  );
+  expect(result.props.children[1]).toEqual(
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  );
 });
